e024fe8702983b10a7d37a9578a1927902cde0be
Norbert4sTao <norbertas@taotesting.com>
2018-09-20 16:54:16 +0200

    first commit

---
+++ controller/Hello.php
@@ -0,0 +1,10 @@
+<?php
+
+class Hello extends \tao_actions_CommonModule {
+  public function myName() {
+      $this->setData('myName', 'tao');
+      $this->setView('myName.tpl');
+  }
+}
+
+?>
---
+++ views/templates/Hello/myName.tpl
@@ -0,0 +1,3 @@
+<div class="main-container tao-name">
+    <h1>Hello my name is <?=(get_data('name'))?></h1>
+</div>
